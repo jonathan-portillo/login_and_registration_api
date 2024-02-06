225ba6c8857bf8be26e3c5b245b0011ac746f7e8
jonathan-portillo <54336208+jonathan-portillo@users.noreply.github.com>
2024-02-06 00:39:31 -0500

    tables are starting to be created

--- data/migrations/20240206052310_authentication.js
+++ data/migrations/20240206052310_authentication.js
@@ -1,3 +1,12 @@
-exports.up = function (knex) {};
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (tbl) => {
+    tbl.increments();
+    tbl.string("username", 128).notNullable().unique().index();
+    tbl.string("password", 128).notNullable().index();
+    tbl.string("email", 128).notNullable().unique();
+    tbl.string("full_name", 128);
+    tbl.string("DOB", 128).notNullable();
+  });
+};
 
 exports.down = function (knex) {};
